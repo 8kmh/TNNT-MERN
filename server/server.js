dd417873c7a4c13523b56e50ede5d1eec606726c
Maida <maida.maxime@gmail.com>
2023-01-29 02:47:22 +0100

    Mongoose Install OK

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const workoutRoutes = require("./routes/workouts");
 const app = express();
 
 // Middleware
+
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
