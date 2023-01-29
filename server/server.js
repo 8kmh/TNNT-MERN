f65d0362127e1da8980cb6eb588da6a930b96381
Maida <maida.maxime@gmail.com>
2023-01-29 02:24:16 +0100

    Router to : /api/workouts OK

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 // Express app
 const app = express();
@@ -12,9 +13,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.get("/", (req, res) => {
-  res.json({ msg: "welcome to the app!" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 // Listen for requests
 app.listen(process.env.PORT, () => {
