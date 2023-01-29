63c2698624d65b5ff8b6ca9f82da238e0d7008ef
Maida <maida.maxime@gmail.com>
2023-01-29 01:46:08 +0100

    Add First Middleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// Middleware
+app.get("/", (req, res, next) => {
+  console.log("toto");
+  next();
+});
+
 // Routes
 app.get("/", (req, res) => {
   res.json({ msg: "welcome to the app!" });
