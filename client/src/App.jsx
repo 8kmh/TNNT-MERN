97152d2c4b57a623f1374207d633f99eb2ea42ef
Maida <maida.maxime@gmail.com>
2023-01-30 15:04:18 +0100

    Navbar component + css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import reactLogo from "./assets/react.svg";
 // Pages & Components
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
