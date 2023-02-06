4db771ce80a23cb56b6475acd5206d1c0bb46869
Maida <maida.maxime@gmail.com>
2023-02-06 20:51:32 +0100

    Add Delete --> error : Uncaught (in promise) SyntaxError: JSON.parse:

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -56,7 +56,7 @@ const deleteWorkout = async (req, res) => {
     return res.status(404).json({ error: "No such workout" });
   }
 
-  res.status(200).json({ workout });
+  res.status(200).json(workout);
 };
 
 // update a workout
@@ -78,7 +78,7 @@ const updateWorkout = async (req, res) => {
     return res.status(404).json({ error: "No such workout" });
   }
 
-  res.status(200).json({ workout });
+  res.status(200).json(workout);
 };
 
 // export
